5ed4423076404efd711e507d78028f417573a6bc
Petter <petter.egesund@gmail.com>
2020-04-28 16:28:03 +0200

    works with channels

--- src/main.rs
+++ src/main.rs
@@ -6,59 +6,107 @@ extern crate rand;
 // use std::sync::Arc;
 // use fnv::FnvHashMap;
 // use im::hashmap::HashMap;
-use std::time::Instant;
-use rand::Rng;
-use evmap;
+// use std::time::Instant;
+// use rand::Rng;
+// use evmap;
 use std::thread;
 use std::sync::Arc;
 use threadpool::ThreadPool;
+use crossbeam_channel::bounded;
+use std::time::Duration;
+// use std::collections::BinaryHeap;
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+#[derive(Clone)]
+enum SearchCommands {
+    Update,
+    Search,
+    Die
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+struct SearchCommand {
+    command: SearchCommands,
+    person_id: String,
+    param: String,
+    result: Option<String>
+}
 
 
 
 fn main() {
-    let pool = ThreadPool::with_name("worker".into(), 2);
-    let mut bvec = Vec::new();
-    bvec.push(88);
-    let mut vec = Arc::new(bvec);
-    loop {
-        for _i in 0..10 {
-            let x = vec.clone();
-            let _handler = pool.execute(move || {
-                println!("Here is the vector: {:?}", &x);
-                
-            });      
-        }
-        pool.join();
-        let n = Arc::get_mut(&mut vec).unwrap();
-        n.push(99);
 
-        println!("The clone: {:?}", vec.clone());
+    let (channel_sender, channel_receiver) = bounded(1000);
+    let add_command = SearchCommand {
+        command: SearchCommands::Update,
+        person_id:  String::from("petter"),
+        param:  String::from("book"),
+        result: None
+    };
+    thread::spawn(move || 
+        executor_loop(channel_receiver)
+    );
+    for _ in 0..10 {
+        channel_sender.send(add_command.clone()).unwrap();
     }
+    let read_command = SearchCommand {
+        command: SearchCommands::Search,
+        person_id:  String::from("petter"),
+        param:  String::from("book"),
+        result: None
+    };
+
+    
+
+    thread::sleep(Duration::from_secs(2));
+    channel_sender.send(read_command).unwrap();
+    loop {}
 }
 
-#[allow(dead_code)]
-fn main2() {
 
-    let (reader, mut writer) = evmap::new();
-    for i in 0..10000000i64 {
-        writer.insert(i.to_string(), i);
+fn executor_loop(receiver: crossbeam_channel::Receiver<SearchCommand>) {
+
+    let pool = ThreadPool::with_name("worker".into(), 2);
+    let bvec = Vec::new();
+    let mut vec = Arc::new(bvec);
+    loop {
+        let mut commands: Vec<SearchCommand>;
+        loop {
+            commands = receiver.try_iter().collect();
+            if commands.len() > 0 { break };    
+        }
+        
+        println!("Got something: {:?}", commands);
+        let read_commands: Vec<SearchCommand> = commands.iter().filter(|c| c.command == SearchCommands::Search).cloned().collect();
+        let write_commands: Vec<SearchCommand> = commands.iter().filter(|c| c.command != SearchCommands::Search).cloned().collect();
+
+        for read_command in read_commands {
+            let cloned_vec = vec.clone();
+            match read_command.command {
+                SearchCommands::Search => {
+                    let _handler = pool.execute(move || {
+                        println!("Here is the vector: {:?}", &cloned_vec);
+                    });      
+                },
+                _ => println!("Should never happend, non read command in read")
+            }
+        }
+        pool.join();
+        
+        let mut_vec = Arc::get_mut(&mut vec).unwrap();
+        for write_command in write_commands {
+            match write_command.command {
+                SearchCommands::Update => {
+                    mut_vec.push(write_command.param.clone());                    
+                    println!("Pushed: {:?}", &write_command.param);
+                },
+                SearchCommands::Die => break,
+                _ => println!("Should never happend, 
+                non read command in read")
+            }
+        }
     }
-    let mut rng = rand::thread_rng();
-    writer.refresh();
-    let start = Instant::now();
-    for j in 0..10 {
-        println!("Number: {0}", j);
-        for i in 0..10000000i64 {
-            let the_clone = reader.clone();
-            let r = rng.gen_range(0, 10000000i64);
-            let res = the_clone.get(&r.to_string());
-            if i == 500 {
-                println!("Value is: {:?}", res);
-            }}
-    }
-    let elapsed = start.elapsed();
-    println!("Debug: {:?}", elapsed);
-    println!("Done");
-    
 }
 
